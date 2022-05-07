1c0ccb7fa3cf5f8ed5fe847d3e80896e0c2bba3f
MarioGalaviz <mga.375@gmail.com>
2022-05-07 12:40:10 -0500

    k8s para api del sitio

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pgUser = process.env.PGUSER
-const pgHost = 'cafeel-site-postgres-1' // process.env.PGHOST
+const pgHost = process.env.PGHOST
 const pgDatabase = process.env.PGDATABASE
 const pgPassword = process.env.PGPASSWORD
 const pgPort = process.env.PGPORT
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 docker build -t mariogalaviz/cafeel-site:latest -t mariogalaviz/cafeel-site:$SHA -f ./site/Dockerfile ./site
+docker build -t mariogalaviz/cafeel-site-api:latest -t mariogalaviz/cafeel-site-api:$SHA -f ./api/Dockerfile ./api
 
 docker push mariogalaviz/cafeel-site:latest
 docker push mariogalaviz/cafeel-site:$SHA 
+docker push mariogalaviz/cafeel-site-api:latest
+docker push mariogalaviz/cafeel-site-api:$SHA 
 
 kubectl apply -f k8s
 kubectl set image deployments/cafeel-site-deployment site=mariogalaviz/cafeel-site:$SHA 
+kubectl set image deployments/cafeel-site-api-deployment site=mariogalaviz/cafeel-site-api:$SHA 
