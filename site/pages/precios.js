063af957f3db1b3390c92ce99a5e9bdbbd0367e8
MarioGalaviz <mga.375@gmail.com>
2022-07-22 07:01:29 -0500

    agregar plan gratuito y lista de features

--- site/pages/precios.js
+++ site/pages/precios.js
@@ -12,44 +12,71 @@ export default function Precios() {
 
     const features = [
         {
+            id: 0,
+            descipcion: 'Cortes de caja',
+        },
+        {
             id: 1,
-            descipcion: 'Por definir',
+            descipcion: 'Cuentas de empleado: ',
         },
         {
             id: 2,
-            descipcion: 'Por definir',
+            descipcion: 'Cocinas: ',
         },
         {
             id: 3,
-            descipcion: 'Por definir',
+            descipcion: 'Control de inventarios',
         },
         {
             id: 4,
-            descipcion: 'Por definir',
+            descipcion: 'Sitio web y menú digital',
+        },
+        {
+            id: 5,
+            descipcion: 'Tarjetas de regalo y programas de lealtad',
+        },
+        {
+            id: 6,
+            descipcion: 'Notificaciones de negocio por WA',
         },
     ]
 
     const planes = [
         {
+            id: 0,
+            nombre: 'Gratuito',
+            descripcion: 'Funciones básicas gratuitas de por vida',
+            costo: 0,
+            features: 3,
+            empleados: 1,
+            cocinas: 1
+        },
+        {
             id: 1,
             nombre: 'Básico',
             descripcion: 'El plan perfecto para negocios de pocos empleados',
             costo: 100,
-            features: [ ]
+            features: 3,
+            empleados: 3,
+            cocinas: 1
         },
         {
             id: 2,
             nombre: 'Pro',
             descripcion: 'El mejor plan para las necesidades regulares de un negocio mediano',
             costo: 300,
-            features: [ ]
+            features: 5,
+            empleados: 5,
+            cocinas: 3
         },
         {
             id: 3,
             nombre: 'Plus',
             descripcion: 'Para los que no quieren límites',
             costo: 500,
-            features: [ ]
+            features: 7,
+            empleados: 'ilimitadas',
+            cocinas: 'ilimitadas'
         },
     ]
     return(
@@ -65,10 +92,10 @@ export default function Precios() {
                 <Box maxWidth='1200px' width='100%'>
                     <Grid container justifyContent='center'>
                         <Grid item xs={11}>
-                            <Typography variant={device > 0 ? 'h2' : 'h3'} align='center' sx={{ my: 2 }} fontWeight='bold'>Llegaste temprano</Typography>
+                            <Typography variant={device > 0 ? 'h2' : 'h3'} align='center' sx={{ my: 2 }}>El <Box fontWeight='bold' display='inline'>plan perfecto</Box> para cualquier negocio</Typography>
                         </Grid>
                         <Grid item xs={11}>
-                            <Typography variant={device > 0 ? 'h4' : 'h5'} align='center' sx={{ my: 2 }}>Estamos en prueba piloto y todos nuestros planes son gratuitos</Typography>
+                            <Typography variant={device > 0 ? 'h5' : 'h6'} align='center' sx={{ my: 2 }}>La única herramienta que te ayuda más de lo que cobra</Typography>
                         </Grid>
                         <Grid item xs={11}>
                             <Box display='flex' justifyContent='space-around' flexWrap='wrap'>
@@ -96,13 +123,13 @@ export default function Precios() {
                                             <Grid item xs={10} sx={{ my: 3 }}>
                                                 <Divider />
                                             </Grid>
-                                            <Grid item xs={9} sx={{ my: 1 }}>
+                                            <Grid item xs={10} sx={{ my: 1 }}>
                                                 {features.map(feature => (
                                                     <Box key={feature.id}>
-                                                        {plan.features.includes(feature.id) ?
+                                                        {plan.features > feature.id ?
                                                         <Box sx={{ my: 2 }} display='flex' key={feature.id}>
                                                             <Bullet color='primary.main'/>
-                                                            <Typography variant='h6' sx={{ mx: 1 }}>{feature.descipcion}</Typography>
+                                                            <Typography variant='h6' sx={{ mx: 1 }}>{feature.descipcion}{feature.id === 1 && plan.empleados}{feature.id === 2 && plan.cocinas}</Typography>
                                                         </Box>
                                                         :
                                                         <Box sx={{ my: 2, opacity: '0.4' }} display='flex' key={feature.id}>
